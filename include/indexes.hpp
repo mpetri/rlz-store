ad7901864e40c23b027455c7076280b31019f490
Matthias Petri <Matthias.Petri@gmail.com>
2015-04-09 16:45:56 +1000

    clearer definition of rlz template params

--- include/indexes.hpp
+++ include/indexes.hpp
@@ -11,14 +11,19 @@
 #include <sdsl/bit_vectors.hpp>
 #include <sdsl/optpfor_vector.hpp>
 
-using dict_creation_strategy = dict_random_sample_budget<32,1024>;
+/* parameters */
+const uint32_t dictionary_mem_budget_mb = 32;
+const uint32_t factorization_block_size = 2048;
+using dict_creation_strategy = dict_random_sample_budget<dictionary_mem_budget_mb,1024>;
 using dict_pruning_strategy = dict_prune_none;
-using csa_type = sdsl::csa_wt< sdsl::wt_huff<sdsl::bit_vector_il<64>> , 4 , 4096 >;
-using dict_index_type = dict_index_csa<csa_type>;
-const uint32_t factorization_block_size = 2*2048;
 using factor_selection_strategy = factor_select_first;
 using factor_encoder = factor_coder<coder::vbyte,coder::vbyte>;
 using block_map = block_map_uncompressed;
+
+
+/* dict type = csa */
+using csa_type = sdsl::csa_wt< sdsl::wt_huff<sdsl::bit_vector_il<64>> , 4 , 4096 >;
+using dict_index_type = dict_index_csa<csa_type>;
 using rlz_type_standard = rlz_store_static<
 	                        dict_creation_strategy,
 	                        dict_pruning_strategy,
