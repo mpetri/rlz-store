4bf76263f828ca7351c485f2839e4eab44fa5cd9
kewenl <kewen.liao@gmail.com>
2015-06-25 23:08:51 +1000

    added new func

---
+++ rlz/rlz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rlz
+//
+//  Created by kewenliao on 22/06/2015.
+//  Copyright (c) 2015 kewenliao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
