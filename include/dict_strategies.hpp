5bb4c6787c51bb608c2657ce10109746abc91c2a
kewenl <kewen.liao@gmail.com>
2015-06-30 14:31:09 +1000

    added coveratge strats

--- include/dict_strategies.hpp
+++ include/dict_strategies.hpp
@@ -5,3 +5,6 @@
 #include "dict_hybrid_sample_budget.hpp"
 #include "dict_prune_none.hpp"
 #include "dict_random_uniform_hash.hpp"
+#include "dict_local_coverage_rolling.hpp"
+#include "dict_local_coverage_disjoint.hpp"
+#include "dict_global_coverage_disjoint.hpp"
