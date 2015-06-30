ca140ff4e2ad77b44d267525c35a71e037393a88
kewenl <kewen.liao@gmail.com>
2015-06-30 14:36:49 +1000

    fixed merge

--- include/dict_strategies.hpp
+++ include/dict_strategies.hpp
@@ -5,11 +5,7 @@
 #include "dict_hybrid_sample_budget.hpp"
 #include "dict_prune_none.hpp"
 #include "dict_random_uniform_hash.hpp"
-<<<<<<< HEAD
 #include "dict_local_coverage_rolling.hpp"
 #include "dict_local_coverage_disjoint.hpp"
 #include "dict_global_coverage_disjoint.hpp"
-=======
 #include "dict_sketch_topk_cover.hpp"
-
->>>>>>> a5b9b16942e30b12d4b02b66e9096a838e702a6b
