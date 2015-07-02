eb52d3a8fedf7df9922486aa8d362fbc3d16e9e1
kewenl <kewen.liao@gmail.com>
2015-07-02 11:39:21 +1000

    change with thresholding

--- include/dict_global_coverage_disjoint.hpp
+++ include/dict_global_coverage_disjoint.hpp
@@ -40,35 +40,47 @@ public:
 			using sketch_type = count_min_sketch<std::ratio<1, 3000000>,std::ratio<1, 10>>;
 			//chunk_freq_estimator_topk<16,500000,sketch_type> cfe_topk;
 			chunk_freq_estimator<t_estimator_block_size,sketch_type> cfe; //build sketches cfe
-
-			LOG(INFO) << "\t" << "Create dictionary with budget " << budget_mb << " MiB";
-			LOG(INFO) << "\t" << "Block size = " << t_block_size; 
-			LOG(INFO) << "\t" << "Num blocks = " << num_blocks_required; 
-
 			auto num_samples = budget_bytes / t_block_size;
-			auto num_subblocks = t_block_size / t_estimator_block_size;
+			auto num_subblocks = t_block_size - t_estimator_block_size;
             LOG(INFO) << "\tDictionary samples = " << num_samples;
             LOG(INFO) << "\tNum subblocks = " << num_subblocks;
             sdsl::read_only_mapper<8> text(col.file_map[KEY_TEXT]);
 
 			// (1) create frequency estimates
-			{
-				LOG(INFO) << "\t" << "Sketch size = " << cfe.sketch.size_in_bytes()/(1024*1024) << " MiB";
-				LOG(INFO) << "\t" << "Sketch params = {d=" << cfe.sketch.d << ",w=" << cfe.sketch.w << "}";
-				LOG(INFO) << "\t" << "Estimating block frequencies";
-				auto est_start = hrclock::now();
-				for(size_t i=0;i<text.size();i++) {
-					auto sym = text[i];
-					cfe.update(sym);
+			// try to load the estimates instead of recomputing
+			auto sketch_name = file_name(col,size_in_bytes) + "-sketch-" + cfe.type();
+			if (! utils::file_exists(sketch_name) || rebuild ) {
+				LOG(INFO) << "\t" << "Create dictionary with budget " << budget_mb << " MiB";
+				LOG(INFO) << "\t" << "Block size = " << t_block_size; 
+				LOG(INFO) << "\t" << "Num blocks = " << num_blocks_required;
+				{
+					LOG(INFO) << "\t" << "Sketch size = " << cfe.sketch.size_in_bytes()/(1024*1024) << " MiB";
+					LOG(INFO) << "\t" << "Sketch params = {d=" << cfe.sketch.d << ",w=" << cfe.sketch.w << "}";
+					LOG(INFO) << "\t" << "Estimating block frequencies";
+					auto est_start = hrclock::now();
+			        // sdsl::read_only_mapper<8> text(col.file_map[KEY_TEXT]);
+					for(size_t i=0;i<text.size();i++) {
+						auto sym = text[i];
+						cfe.update(sym);
+					}
+					auto est_stop = hrclock::now();
+					auto est_error = cfe.sketch.estimation_error();
+					LOG(INFO) << "\t" << "Estimation time = " << duration_cast<milliseconds>(est_stop-est_start).count() / 1000.0f << " sec";
+					LOG(INFO) << "\t" << "Sketch estimation error = " << est_error;
+					LOG(INFO) << "\t" << "Sketch estimation confidence = " << cfe.sketch.estimation_probability();
+					LOG(INFO) << "\t" << "Sketch noise estimate = " << cfe.sketch.noise_estimate();
+					LOG(INFO) << "\t" << "Maximum frequency = " << cfe.max_freq();
+					// LOG(INFO) << "\t" << "Number of things hashed = " << cfe.sketch.sketch.total_count;
+					sdsl::store_to_file(cfe,sketch_name);
 				}
-				auto est_stop = hrclock::now();
-				auto est_error = cfe.sketch.estimation_error();
-				LOG(INFO) << "\t" << "Estimation time = " << duration_cast<milliseconds>(est_stop-est_start).count() / 1000.0f << " sec";
-				LOG(INFO) << "\t" << "Sketch estimation error = " << est_error;
+			} else {
+				LOG(INFO) << "\t" << "Load sketch from file " << sketch_name;
+				sdsl::load_from_file(cfe,sketch_name);
+				LOG(INFO) << "\t" << "Sketch estimation error = " << cfe.sketch.estimation_error();
 				LOG(INFO) << "\t" << "Sketch estimation confidence = " << cfe.sketch.estimation_probability();
-				//auto topk_blocks = cfe_topk.topk();
-				//threshold = topk_blocks.back().first;
-				//LOG(INFO) << "\t" << "Threshold freq = " << threshold;
+				LOG(INFO) << "\t" << "Sketch noise estimate = " << cfe.sketch.noise_estimate();
+				LOG(INFO) << "\t" << "Maximum frequency = " << cfe.max_freq();
+				// LOG(INFO) << "\t" << "Number of things hashed = " << cfe.sketch.sketch.total_count;
 			}
 
 			// (2) compute uniform max coverage with sketches and write dict 
@@ -82,9 +94,9 @@ public:
 				uint32_t id;
 				uint64_t weight;
 				sdsl::bit_vector bitmask; 
-				block_cover(uint32_t size) {
-					bitmask.resize(size);
-				}
+				// block_cover(uint32_t size) {
+				// 	bitmask.resize(size);
+				// }
 				bool operator<(const block_cover& bc) const {
 					return weight < bc.weight;
 				}
@@ -99,7 +111,8 @@ public:
 			boost_heap cover_pq;
 			std::unordered_map<uint64_t,uint64_t> small_blocks;
 			size_t j=0; //index to bit vector map
-
+			sdsl::bit_vector submask(num_subblocks);
+			uint32_t threshold = 1000;//change it later to quantile threshold as a parameter, simulating top k
 			//First pass, build pq
 			//note the code might have the non-divisible issue, memeroy issue is block size are small
 
@@ -107,25 +120,34 @@ public:
 				auto sym = text[i];
 				auto hash = rk.update(sym);
 
+				if(i % t_block_size > t_block_size - t_estimator_block_size) continue; //skip the last few rolling subblocks
+
 				//dealing with disjoint sub-blocks, rolling after the first block
-				if((i+1)%t_estimator_block_size == 0) {
-					auto est_freq = cfe.estimate(hash);
+				auto est_freq = cfe.estimate(hash);
+				// LOG(INFO) << "\t" << est_freq;
+				// LOG(INFO) << est_freq;
+				if(est_freq >= threshold) { 
 					sum_weights += est_freq;
 					if(small_blocks.find(hash) == small_blocks.end()) {
 						small_blocks[hash] = j++;
 					} 
-
-					if((i+1)%t_block_size == 0) {
-						block_cover cov(num_subblocks);
-						cov.id = block_no;
-						cov.weight = sum_weights;
-						cover_pq.push(cov);
-						//update
-						block_no++;
-						sum_weights = 0;
-					}
+				} else {
+					submask[i%num_subblocks] = 1; //not to be considered in future
+				}
+					
+				if((i+t_estimator_block_size)%t_block_size == 0) {
+					block_cover cov;
+					cov.id = block_no;
+					cov.weight = sum_weights;
+					cov.bitmask = submask;
+					cover_pq.push(cov);
+					//update
+					block_no++;
+					sum_weights = 0;
+					submask = sdsl::bit_vector(num_subblocks);
 				}
 			}
+			LOG(INFO) << "\t" << "Top items = " << small_blocks.size();
 
 			//second pass, do maximum coverage, simulating the lazy approach for set-cover							
 			LOG(INFO) << "\t" << "Perform maximum coverage";
@@ -172,7 +194,8 @@ public:
 					if(needed_update) {
 						/* needed update */
 						//LOG(INFO) << "\t" << "Needed Update!";
-						cover_pq.push(most_weighted_block);
+						if(most_weighted_block.weight > 0)
+							cover_pq.push(most_weighted_block);
 					} else {
 						/* add to picked blocks */				
 						picked_blocks.push_back(most_weighted_block.id);
@@ -186,7 +209,7 @@ public:
 						LOG(INFO) << "\t" << "Blocks weight: " << most_weighted_block.weight;
 					}
 				}
-				LOG(INFO) << "\t" << "Left to cover: " << need_to_cover;
+				LOG(INFO) << "\t" << "Covered small blocks: " << small_blocks.size() - need_to_cover;
 				LOG(INFO) << "\t" << "Covered in heap: " << text.size()/t_block_size-cover_pq.size();
 			}
 			std::sort(picked_blocks.begin(),picked_blocks.end());
